72a24037a876b8a581c55046e0622475f6a6f45e
matheusbalduino <matheus.balduin@gmail.com>
2022-04-27 21:21:19 -0300

    first commit

---
+++ provacsharp3/Controllers/ClienteController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using provacsharp3.context;
+using provacsharp3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provacsharp3.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public ClienteController(MyDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Professors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        // GET: Pessoas/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(c => c.Cliente_id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PessoaExists(long id)
+        {
+            return _context.Clientes.Any(c => c.Cliente_id == id);
+        }
+    }
+}
